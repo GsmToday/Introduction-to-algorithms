925c854c7432d5fe01a75783c816dbb10347b0ca
GsmToday <gaoshimengdlnu@gmail.com>
2014-11-07 17:16:29 +0800

    查找- 02二分查找

---
+++ Chapter 2/BinarySearch.java
@@ -0,0 +1,44 @@
+package preliminary;
+
+public class BinarySearch {
+	private int[] arraytoSort;
+	private int key;
+	private int index;
+	//private InsertionSort i; 
+	
+	// 通过构造函数 初始化待排元素数组（private 成员）
+	BinarySearch(int[] arraytoSort, int key){
+		this.arraytoSort = arraytoSort;
+		this.key = key;
+	}
+	/* 如果想用排序
+	public void toSort(){
+		i = new InsertionSort(arraytoSort);
+		i.sort();
+		//i.show();
+	}
+	*/
+	//如果search到了返回index
+	public int search(int start, int end){
+		
+		if(start <= end){
+			int mid = (start + end)/2;
+			if(arraytoSort[mid] > key)
+				search(start,mid-1);
+			else if(arraytoSort[mid] < key)
+				search(mid+1,end);
+			else 
+				index = mid;
+		}
+		else 
+			index = -1;
+		return index+1;
+	}
+	public static void main(String[] args) {
+		int a[] ={5,2,4,6,1,3};
+		//InsertionSort i = new InsertionSort(a);
+		BinarySearch e = new BinarySearch(a,4);
+		//e.toSort();
+		System.out.print(e.search(0,a.length));
+		}
+}
